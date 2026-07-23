debb2f0db9c29e3c00d70b99f30485a044356ff2
Tomas Ambrosini <tomasambrosini28@gmail.com>
2026-07-23 12:08:50 -0400

    fix(rbac): grant FINANCE department access to Sales Hub

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,7 @@ function MainRouter() {
           
           {/* MANAGER DOMAINS */}
           <Route path="catalog/*" element={<RoleRoute><Catalog /></RoleRoute>} />
-          <Route path="sales/*" element={<RoleRoute allowedRoles={['SUPER_ADMIN', 'DIRECTOR', 'MANAGER', 'COORDINATOR']} allowedDepartments={['SALES', 'EXECUTIVE', 'INSIDE_SALES']}><Sales /></RoleRoute>} />
+          <Route path="sales/*" element={<RoleRoute allowedRoles={['SUPER_ADMIN', 'DIRECTOR', 'MANAGER', 'COORDINATOR']} allowedDepartments={['SALES', 'EXECUTIVE', 'INSIDE_SALES', 'FINANCE']}><Sales /></RoleRoute>} />
           <Route path="dispatch" element={<RoleRoute allowedRoles={['SUPER_ADMIN', 'DIRECTOR', 'MANAGER', 'COORDINATOR']}><DispatchHub /></RoleRoute>} />
 
           {/* SUPER ADMIN EXCLUSIVE DOMAINS */}
